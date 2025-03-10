d24f0769a8b7e2b16bd98b408de7618e114979c4
Mahdi <mahdi.mousavi0011@gmail.com>
2025-03-10 13:00:16 +0100

    Delete and edit added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 
+import { useState } from 'react'
 import './App.scss'
-import Button from './components/button/button'
 import Card from './components/card/card'
 import Input from './components/input/input'
 
 function App() {
-
+  const[List, setList] = useState("");
+  const AddToList = () => {
+    setList(List+1)
+  }
 
   return (
     <>
-    <Input />
-    <Button />
+    <Input AddToList={AddToList}/>
     <Card />
     </>
   )
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,12 +1,50 @@
+import React, { useState } from "react"
 import "./input.scss"
 
+
+
 export default function Input() {
+  const [inputData, setInputData] = useState("")
+  const [List, setList] = useState<string[]>([]);
+
+  const AddToList = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setList((prevList) => [...prevList, inputData]);
+    setInputData("");
+  }
+
+  const removeFromList = (OneOfList: string) => {
+    setList(List.filter((item) => item !== OneOfList));
+  }
+
+  const editList = (todo: string) => {
+    setList(List.map((a) => (a === todo ? inputData : a)))
+  }
   return (
     <div>
     <input 
     type="text"
     placeholder='write your list...'
-      />
+    onChange={(e)=>{setInputData(e.target.value)}}
+    value={inputData}
+    />
+    <button 
+    onClick={AddToList}>
+    Add!
+    </button>
+    <div>
+    <p>you wrote:{List}</p>
+    </div>
+    <div>
+      <ul>
+      {List.map((item, index) => (
+                    <li key={index}>{item}
+                     <button onClick={() => removeFromList(item)}>delete</button>
+                     <button onClick={() => editList(OneOfList)}>Edit</button>
+                    </li>
+                ))}
+   
+      </ul>
+    </div>
     </div>
   )
 }
