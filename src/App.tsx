23f189ba233221b6df84b26d1f3e5acf16a923f9
Mahdi <mahdi.mousavi0011@gmail.com>
2025-03-11 15:15:00 +0100

    NO Edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 
-import { useState } from 'react'
 import './App.scss'
 import Card from './components/card/card'
 import Input from './components/input/input'
 
+
 function App() {
-  const[List, setList] = useState("");
-  const AddToList = () => {
-    setList(List+1)
-  }
+
 
   return (
     <>
-    <Input AddToList={AddToList}/>
+    <Input />
     <Card />
     </>
   )
