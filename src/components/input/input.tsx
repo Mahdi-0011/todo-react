d6a9820542c61705d93d228ea33c97ef7c004b64
Mahdi <mahdi.mousavi0011@gmail.com>
2025-03-11 18:24:53 +0100

    edit added.

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,50 +1,64 @@
-import React, { useState } from "react"
-import "./input.scss"
-
-
+import  { useState } from "react";
+import "./input.scss";
 
 export default function Input() {
-  const [inputData, setInputData] = useState("")
+  const [inputData, setInputData] = useState("");
   const [List, setList] = useState<string[]>([]);
+  const [editingItem, setEditingItem] = useState<string | null>(null);
 
-  const AddToList = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setList((prevList) => [...prevList, inputData]);
+  const AddOrUpdateItem = () => {
+    if (!inputData.trim()) return;
+
+    if (editingItem !== null) {
+      setList((prevList) =>
+        prevList.map((item) => (item === editingItem ? inputData : item))
+      );
+      setEditingItem(null);
+    } else {
+      setList((prevList) => [...prevList, inputData]);
+    }
+
     setInputData("");
-  }
+  };
 
   const removeFromList = (OneOfList: string) => {
-    setList(List.filter((item) => item !== OneOfList));
-  }
+    setList((prevList) => prevList.filter((item) => item !== OneOfList));
+  };
 
   const editList = (todo: string) => {
-    setList(List.map((a) => (a === todo ? inputData : a)))
-  }
+    setInputData(todo);
+    setEditingItem(todo);
+  };
+
   return (
     <div>
-    <input 
-    type="text"
-    placeholder='write your list...'
-    onChange={(e)=>{setInputData(e.target.value)}}
-    value={inputData}
-    />
-    <button 
-    onClick={AddToList}>
-    Add!
-    </button>
-    <div>
-    <p>you wrote:{List}</p>
+      <input
+        type="text"
+        placeholder="write your list..."
+        onChange={(e) => setInputData(e.target.value)}
+        value={inputData}
+      />
+      <button type="button" onClick={AddOrUpdateItem}>
+        {editingItem !== null ? "Update" : "Add!"}
+      </button>
+      <div>
+        <p>you wrote: {List.join(", ")}</p>
+      </div>
+      <div>
+        <ul>
+          {List.map((item) => (
+            <li key={item}>
+              {item}
+              <button type="button" onClick={() => removeFromList(item)}>
+                Delete
+              </button>
+              <button type="button" onClick={() => editList(item)}>
+                Edit
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    <div>
-      <ul>
-      {List.map((item, index) => (
-                    <li key={index}>{item}
-                     <button onClick={() => removeFromList(item)}>delete</button>
-                     <button onClick={() => editList(OneOfList)}>Edit</button>
-                    </li>
-                ))}
-   
-      </ul>
-    </div>
-    </div>
-  )
+  );
 }
